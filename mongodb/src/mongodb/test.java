7e8d3902b6d9162aff47820ca7760ff5bfce4784
cklogic <chuanzyz@sina.com>
2017-05-06 20:40:58 +0800

    test

--- mongodb/src/mongodb/test.java
+++ mongodb/src/mongodb/test.java
@@ -8,10 +8,8 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try{   
-		       // 连接到 mongodb 服务
-		         MongoClient mongoClient = new MongoClient( "123.207.221.15" , 27017 );
 		       
-		         // 连接到数据库
+		         MongoClient mongoClient = new MongoClient( "123.207.221.15" , 27017 );
 		         MongoDatabase mongoDatabase = mongoClient.getDatabase("test");  
 		       System.out.println("Connect to database successfully");
 		        
